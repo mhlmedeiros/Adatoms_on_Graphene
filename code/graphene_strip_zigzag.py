b9c67157602ba84882366b3b3259f252c9f16840
Marcos <mhlmedeiros@gmai.com>
2021-09-14 17:14:39 +0200

    Start a script to study the zigzag system

---
+++ code/graphene_strip_zigzag.py
@@ -0,0 +1,21 @@
+import kwant
+import tinyarray
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from itertools import product
+
+FONT_LABELS = 20
+font = {'family' : 'serif', 'weight' : 'bold', 'size': FONT_LABELS}
+font = {'size': FONT_LABELS}
+mpl.rc('font', **font)
+plt.rc('text', usetex=True)
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

c99074c2b37759af02e1f27c280eac1f1b9af376
Marcos <mhlmedeiros@gmai.com>
2021-09-15 16:42:48 +0200

    Enable transmission calculation

--- code/graphene_strip_zigzag.py
+++ code/graphene_strip_zigzag.py
@@ -34,10 +34,8 @@ def hopping_colors(site1, site2):
         color='black'
     return color
 
-
 def hopping_lw(site1, site2):
     return 0.01 if site1.family == site2.family else 0.07
-
 
 def site_size_function(site):
     if site.family == bm.HA or site.family == bm.HB:
@@ -52,8 +50,8 @@ def main():
     adatom_concentration = 0.125  # [% of Carbon atoms]
 
     ## Define the shape of the system:
-    system_width  = 50
-    system_length = 100
+    system_width  = 5
+    system_length = 10
     shape = bm.Rectangle(width=system_width, length=system_length)
 
     ## Build the scattering region:
@@ -71,7 +69,7 @@ def main():
     # adatom_F_params = dict(T = 5.5, eps = -2.2, Lambda_I = 3.3e-3, Lambda_BR = 11.2e-3, Lambda_PIA = -7.3e-3)
     system = bm.insert_adatoms_randomly(system, shape, adatom_concentration, adatom_params=adatom_H_params)
 
-    
+
     fig, ax = plt.subplots(figsize=(20,5))
     kwant.plot(system,
                site_color=family_colors_H,
@@ -81,6 +79,26 @@ def main():
                site_lw=0.1, ax=ax)
     ax.set_aspect('equal')
     # ax.axis('off')
+    plt.show()
+
+    # Calculate the transmission
+    Bflux = 0     # in units of quantum of magnetic flux
+    Bfield = Bflux / (np.sqrt(3)/2)
+    parameters_hand = dict(V=0,
+                           t=2.6,
+                           B=Bfield,
+                           eps_H=0.16,
+                           peierls=bm.peierls_scatter,
+                           peierls_lead_L=bm.peierls_lead_L,
+                           peierls_lead_R=bm.peierls_lead_R,
+                           Lm=0,
+                        )
+    energy_values = np.linspace(-0.5,0.5,300)
+    transmission1 = bm.calculate_conductance(system, energy_values, params_dict=parameters_hand)
+    #
+    # #
+    plt.plot(energy_values, transmission1)
+    # # plt.plot(energy_values, transmission2)
     plt.show()
 
 
